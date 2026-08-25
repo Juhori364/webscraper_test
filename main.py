25554c97bc43eee0397bb1aa4dc5791cf0d686f0
User <juho.rimpelainen@gmail.com>
2026-08-25 22:17:19 +0300

    Added function to scrape multiple links

--- main.py
+++ main.py
@@ -2,47 +2,35 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 
-url = "https://www.imperial-library.info/content/doors-oblivion"
-response = requests.get(url)
+def getcontent(url):
 
-soup = BeautifulSoup(response.text, 'html.parser')
-#print(soup.prettify()) 
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    #print(soup.prettify()) 
 
-main = soup.find('div', class_='entry-content clear')
+    main = soup.find('div', class_='entry-content clear')
+    for br in main.find_all('br'):
+        br.replace_with('\n')
+    for quotes in main.find_all('’'):
+        br.replace_with('"')
 
+    text = main.get_text(separator="\n\n")
+    text = "\n".join(line.rstrip() for line in text.splitlines() if line.strip() != "")
+    print(text)
 
-for br in main.find_all('br'):
-    br.replace_with('\n')
+    with open("demofile.txt", "a", encoding="utf-8") as f:
+        f.write(text)
 
-for quotes in main.find_all('’'):
-    br.replace_with('"')
+def getcontent_cycle(baseurl):
+    response = requests.get(baseurl)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    #print(soup.prettify()) 
+    
+    main = soup.find_all('a', class_='title')
 
-
-text = main.get_text(separator="\n\n")
-
-
-text = "\n".join(line.rstrip() for line in text.splitlines() if line.strip() != "")
-
-print(text)
-"""
-content = soup.find('div', class_='entry-content')
-
-for br in content.find_all('br'):
-    br.replace_with('\n')
-
-parts = []
-for p in content.find_all('p'):
-    parts.append(p.get_text())
-
-text = "\n\n".join(parts)
-
-text = content.get_text()
-print(text)
-
-"""
+    for link in main:
+        getcontent(link["href"])
+    
 
 
-
-
-with open("demofile.txt", "a", encoding="utf-8") as f:
-    f.write(text)
+getcontent_cycle("https://www.imperial-library.info/game-books/tes5-skyrim-books")
